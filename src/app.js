854729998b85e43014d3ab204207bd02bcc589c7
bijangh <b.ghanbari.it@gmail.com>
2021-06-10 18:26:53 +0430

    add mentions apii

--- src/app.js
+++ src/app.js
@@ -1,16 +1,68 @@
-const fastify = require("fastify")({ logger: true });
+/* eslint-disable no-unused-vars */
+const fastify = require("fastify")({});
+import axios from "axios";
 
 fastify.register(require("fastify-cors"), { origin: false });
 
-fastify.get("/", (req, reply) => {
-  reply.send({ hello: "world" });
+fastify.post("/tweets", async (req, reply) => {
+  if (!req.body.user)
+    return new Error(`please add user to the body of the request`);
+
+  const {
+    data: { data: userData },
+  } = await axios.get(
+    `https://api.twitter.com/2/users/by/username/${req.body.user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  const {
+    data: { data: userTweets },
+  } = await axios.get(
+    `https://api.twitter.com/2/users/${userData.id}/tweets?tweet.fields=id,text,created_at,conversation_id&max_results=2 `,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  return userTweets ?? [];
 });
 
-fastify.post("/parrot", (req, reply) => {
-  reply.send(req.body);
+fastify.post("/mentions", async (req, reply) => {
+  if (!req.body.user)
+    return new Error(`please add user to the body of the request`);
+
+  const {
+    data: { data: userData },
+  } = await axios.get(
+    `https://api.twitter.com/2/users/by/username/${req.body.user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  const {
+    data: { data: mentionedTweets },
+  } = await axios.get(
+    `https://api.twitter.com/2/users/${userData.id}/mentions?tweet.fields=id,author_id,text,created_at&user.fields=id,name,profile_image_url&max_results=10 `,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }
+  );
+
+  return mentionedTweets ?? [];
 });
 
-fastify.get("/health-check", (req, reply) => {
+fastify.all("/health-check", (req, reply) => {
   reply.send(true);
 });
 
