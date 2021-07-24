4232bd39619376e9e461209dca5c2262bb70bc69
bijangh <b.ghanbari.it@gmail.com>
2021-07-24 22:10:41 +0430

    parse shit

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@
 const fastify = require("fastify")({});
 import axios from "axios";
 
-fastify.register(require("fastify-cors"), { origin: false });
+fastify.register(require("fastify-cors"), {
+  origin: process.env.NODE_ENV === "production" ? false : true,
+});
 
 fastify.post("/tweets", async (req, reply) => {
-  if (!req.body.user)
-    return new Error(`please add user to the body of the request`);
+  const { user } = JSON.parse(req.body);
+
+  if (!user) return new Error(`please add user to the body of the request`);
 
   const {
     data: { data: userData },
   } = await axios.get(
-    `https://api.twitter.com/2/users/by/username/${req.body.user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
+    `https://api.twitter.com/2/users/by/username/${user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
@@ -22,7 +25,7 @@ fastify.post("/tweets", async (req, reply) => {
   const {
     data: { data: userTweets },
   } = await axios.get(
-    `https://api.twitter.com/2/users/${userData.id}/tweets?tweet.fields=id,text,created_at,conversation_id&max_results=2 `,
+    `https://api.twitter.com/2/users/${userData.id}/tweets?tweet.fields=id,text,created_at,conversation_id&max_results=10 `,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
@@ -30,17 +33,18 @@ fastify.post("/tweets", async (req, reply) => {
     }
   );
 
-  return userTweets ?? [];
+  return { user: userData, tweets: userTweets };
 });
 
 fastify.post("/mentions", async (req, reply) => {
-  if (!req.body.user)
-    return new Error(`please add user to the body of the request`);
+  const { user } = JSON.parse(req.body);
+
+  if (!user) return new Error(`please add user to the body of the request`);
 
   const {
     data: { data: userData },
   } = await axios.get(
-    `https://api.twitter.com/2/users/by/username/${req.body.user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
+    `https://api.twitter.com/2/users/by/username/${user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
@@ -59,7 +63,7 @@ fastify.post("/mentions", async (req, reply) => {
     }
   );
 
-  return mentionedTweets ?? [];
+  return { user: userData, tweets: mentionedTweets };
 });
 
 fastify.all("/health-check", (req, reply) => {
@@ -69,7 +73,9 @@ fastify.all("/health-check", (req, reply) => {
 export default async () => {
   try {
     await fastify.listen(process.env.PORT || 4000, "0.0.0.0");
+    console.log("shit started successfully");
   } catch (err) {
+    console.log("🚀 ~ file: app.js ~ line 73 ~ err", err);
     fastify.log.error(err);
     process.exit(1);
   }
