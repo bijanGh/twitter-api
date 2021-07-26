fc4d051f5fab4358302828997f9ed381de712de1
bijangh <b.ghanbari.it@gmail.com>
2021-07-26 18:23:46 +0430

    add get user

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ fastify.register(require("fastify-cors"), {
   origin: process.env.NODE_ENV === "production" ? false : true,
 });
 
+fastify.post("/getUser", async (req, reply) => {
+  const { user } = JSON.parse(req.body);
+
+  if (!user) return new Error(`please add user to the body of the request`);
+
+  const {
+    data: { data: userData },
+  } = await axios.get(
+    `https://api.twitter.com/2/users/by/username/${user}?user.fields=id,name,profile_image_url&tweet.fields=id,text,created_at,conversation_id `,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    }
+  );
+  return userData ?? undefined;
+});
+
 fastify.post("/tweets", async (req, reply) => {
   const { user } = JSON.parse(req.body);
 
@@ -55,7 +73,7 @@ fastify.post("/mentions", async (req, reply) => {
   const {
     data: { data: mentionedTweets },
   } = await axios.get(
-    `https://api.twitter.com/2/users/${userData.id}/mentions?tweet.fields=id,author_id,text,created_at&user.fields=id,name,profile_image_url&max_results=10 `,
+    `https://api.twitter.com/2/users/${userData.id}/mentions?tweet.fields=id,author_id,text,created_at&user.fields=id,name,profile_image_url&max_results=100 `,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
